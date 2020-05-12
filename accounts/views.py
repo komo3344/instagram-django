30e7971a0bbfae964e4c5b1ddc2b8b8abce61171
junyoung <jy940608@naver.com>
2020-05-13 03:05:00 +0900

    Logout, signup after login

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.views import LoginView, logout_then_login
 from django.contrib import messages
 from . import forms
+
+login = LoginView.as_view(template_name="accounts/login_form.html")
+
+
+def logout(request):
+    messages.success(request, '로그아웃 되었습니다')
+    return logout_then_login(request)
+
 def signup(request):
     if request.method == 'POST':
         form = forms.SignupForm(request.POST)
         if form.is_valid():
             signed_user = form.save()
             messages.success(request, '회원가입이 되었습니다.')
+            auth_login(request, signed_user)
             signed_user.send_welcome_email()  # FIXME: Celery로 처리하는 것을 추천.
             next_url = request.GET.get('next', '/')
             return redirect(next_url)
