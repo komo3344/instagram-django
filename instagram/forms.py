3df024519bdd5239d96ded476b001a69a8c8d142
junyoung <jy940608@naver.com>
2020-05-31 22:07:08 +0900

    Update writing comment on the posting page

--- instagram/forms.py
+++ instagram/forms.py
@@ -14,3 +14,6 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['message']
+        widgets = {
+            'message': forms.Textarea({'rows': 2})
+        }
--- instagram/views.py
+++ instagram/views.py
@@ -23,9 +23,12 @@ def index(request):
                         .exclude(pk=request.user.pk)\
                         .exclude(pk__in=request.user.following_set.all())[:3]
 
+    comment_form = CommentForm()
+
     return render(request, 'instagram/index.html', {
         'post_list': post_list,
-        'suggested_user_list': suggested_user_list
+        'suggested_user_list': suggested_user_list,
+        'comment_form': comment_form
     })
 
 @login_required
@@ -48,8 +51,10 @@ def post_new(request):
 @login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    comment_form = CommentForm()
     return render(request, 'instagram/post_detail.html', {
         'post': post,
+        'comment_form': comment_form,
     })
 
 @login_required
